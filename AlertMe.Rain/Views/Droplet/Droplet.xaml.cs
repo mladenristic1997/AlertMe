e8618a02239dea654e64aa8fe20fe7442536de14
Mladen <mladenristic1997@gmail.com>
2020-03-12 11:34:45 +0100

    animatio nhell

--- AlertMe.Rain/Views/Droplet/Droplet.xaml.cs
+++ AlertMe.Rain/Views/Droplet/Droplet.xaml.cs
@@ -25,36 +25,45 @@ namespace AlertMe.Rain
         public double FallingSpeed { get; set; }
         public RotationOrientation RotationOrientation { get; set; }
         public string ImagePath { get; set; }
+        public Thickness ImagePosition { get; set; }
 
         public Droplet()
         {
             InitializeComponent();
         }
 
+        public void Init()
+        {
+            DropletImage.Source = new BitmapImage(new Uri(ImagePath));
+            DropletImage.Margin = ImagePosition;
+            DropletImage.UpdateLayout();
+        }
+
         public void UpdatePosition()
         {
-            var currentY = Margin.Top;
-            var currentX = Margin.Left;
-            Margin = new Thickness(currentX, currentY + FallingSpeed, 0, 0);
-            //RotateTransform currentAngle = DropletImage.RenderTransform as RotateTransform;
-            //if (RotationOrientation == RotationOrientation.Clockwise)
-            //{
-            //    var rotateAnimation = new DoubleAnimation(currentAngle.Angle, currentAngle.Angle + RotationAngle, new TimeSpan(0, 0, 0, 0, 16));
-            //    var rt = (RotateTransform)DropletImage.RenderTransform;
-            //    rt.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
-            //}
-            //else
-            //{
-            //    var rotateAnimation = new DoubleAnimation(currentAngle.Angle, currentAngle.Angle - RotationAngle, new TimeSpan(0, 0, 0, 0, 16));
-            //    var rt = (RotateTransform)DropletImage.RenderTransform;
-            //    rt.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
-            //}
+            var currentY = DropletImage.Margin.Top;
+            var currentX = DropletImage.Margin.Left;
+            DropletImage.Margin = new Thickness(currentX, currentY + FallingSpeed, 0, 0);
+            SkewTransform currentAngle = DropletImage.RenderTransform as SkewTransform;
+            if (RotationOrientation == RotationOrientation.Clockwise)
+            {
+                var rotateAnimation = new DoubleAnimation(currentAngle.AngleX, currentAngle.AngleX + RotationAngle, new TimeSpan(0, 0, 0, 0, 16));
+                var rt = (RotateTransform)DropletImage.RenderTransform;
+                rt.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
+            }
+            else
+            {
+                var rotateAnimation = new DoubleAnimation(currentAngle.AngleX, currentAngle.AngleX - RotationAngle, new TimeSpan(0, 0, 0, 0, 16));
+                var rt = (RotateTransform)DropletImage.RenderTransform;
+                rt.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
+            }
+            DropletImage.UpdateLayout();
         }
     }
 
     public enum RotationOrientation
     {
-        Clockwise,
-        CounterClockwise
+        Clockwise = 0,
+        CounterClockwise = 1
     }
 }
--- AlertMe.Rain/Views/OverlayView.xaml.cs
+++ AlertMe.Rain/Views/OverlayView.xaml.cs
@@ -34,50 +34,54 @@ namespace AlertMe.Rain
             EventAggregator = ea;
             Player = new MediaPlayer();
             Random = new Random();
-            Player.MediaEnded += (o, e) => { AnimationInProgress = false; Droplets.Clear(); Overlay.Children.Clear(); };
+            Player.MediaEnded += OnMediaEnded;
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        void Button_Click(object sender, RoutedEventArgs e)
         {
-            OnTriggerRain();
+            TriggerRain();
         }
 
-        void OnTriggerRain()
+        async Task TriggerRain()
         {
             if (AnimationInProgress)
                 return;
+            MakeItRainBtn.Visibility = Visibility.Collapsed;
             AnimationInProgress = true;
-            Player.Play();
-            //Droplets = new List<Droplet>();
-            //int generationCounter = 0;
-            //while (AnimationInProgress)
-            //{
-            //    if (generationCounter % 4 == 0)
-            //        AddNewDroplets();
-            //    UpdateWorld();
-            //    await Task.Delay(FrameDuration);
-            //}
+            //Player.Open(new Uri(Environment.CurrentDirectory + "/Resources/rainingmen.mp3", UriKind.Relative));
+            //Player.Play();
+            Droplets = new List<Droplet>();
+            int generationCounter = 0;
+            while (AnimationInProgress)
+            {
+                if (generationCounter % 4 == 0)
+                    AddNewDroplets();
+                UpdateWorld();
+                generationCounter++;
+                await Task.Delay(FrameDuration);
+            }
         }
 
         void AddNewDroplets()
         {
             for (int i = 0; i < Overlay.ActualWidth / 300; i++)
             {
-                var x = Random.NextDouble() * Overlay.ActualWidth;
+                var x = Math.Round(Random.NextDouble() * Overlay.ActualWidth, 2);
                 var margin = new Thickness(x, 0, 0, 0);
-                var fallingSpeed = 2 + Random.NextDouble() * 2;
-                var rotationAngle = 6 + Random.NextDouble() * 3;
+                var fallingSpeed = 2 + ((int)(Random.NextDouble() * 2 * 100) / 100.0);
+                var rotationAngle = 6 + Math.Round(Random.NextDouble() * 3, 2);
                 var rotationOrientation = (RotationOrientation)Random.Next(0, 2);
                 var imagePath = Random.Next(0, 2) == 0 ? "pack://application:,,,/AlertMe.Rain;component/Resources/heart.png" : "pack://application:,,,/AlertMe.Rain;component/Resources/mladen.png";
                 var droplet = new Droplet
                 {
-                    Margin = margin,
+                    ImagePosition = margin,
                     FallingSpeed = fallingSpeed,
                     RotationAngle = rotationAngle,
                     RotationOrientation = rotationOrientation,
                     ImagePath = imagePath
                 };
+                droplet.Init();
                 Droplets.Add(droplet);
                 Overlay.Children.Add(droplet);
             }
@@ -87,6 +91,15 @@ namespace AlertMe.Rain
         {
             foreach (var d in Droplets)
                 d.UpdatePosition();
+            Overlay.UpdateLayout();
+        }
+
+        void OnMediaEnded(object o, EventArgs e)
+        {
+            AnimationInProgress = false; 
+            Droplets.Clear();
+            Overlay.Children.Clear();
+            MakeItRainBtn.Visibility = Visibility.Visible;
         }
     }
 }
